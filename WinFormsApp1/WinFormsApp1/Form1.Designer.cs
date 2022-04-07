bc5b08773a6d68ae13d4edbb50eb748c6d3680fb
Dagore <david.gomez.redondo.dev@gmail.com>
2022-04-07 12:20:51 +0200

    Introducción

--- WinFormsApp1/WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -28,11 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -43,14 +49,13 @@
             this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.button2, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(35, 80);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(37, 23);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(200, 100);
             this.tableLayoutPanel1.TabIndex = 0;
-            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
             // 
             // button1
             // 
@@ -87,16 +92,62 @@
             this.label1.Text = "0";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(40, 149);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(200, 100);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.tableLayoutPanel2.SetColumnSpan(this.label3, 2);
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(3, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(194, 67);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Esperando ...";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.tableLayoutPanel2.SetColumnSpan(this.textBox1, 2);
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PlaceholderText = "Escribe tu nombre";
+            this.textBox1.Size = new System.Drawing.Size(194, 23);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.tableLayoutPanel2);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -107,5 +158,9 @@
         private Button button1;
         private Button button2;
         private Label label1;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Label label3;
+        private TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- WinFormsApp1/WinFormsApp1/Form1.cs
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -6,22 +6,42 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
-
-        //Evento del click del botón
+        //Incrementa valor del label1.
         private void button1_Click (object sender, EventArgs e)
         {
             int i = int.Parse(label1.Text);
             i++;
             label1.Text = i.ToString();
         }
+        //Resetea el valor del label1.
         private void button2_Click (object sender, EventArgs e)
         {
             label1.Text = "0";
         }
-
-        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        //Saluda con el input al hacer Enter.
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
         {
+            if (e.KeyValue == (int) Keys.Enter)
+            {
+                string name = textBox1.Text.Trim();
+                if (name.Length != 0)
+                {
+                    label3.Text = $"Hola {name}";
+                    this.Text = "";
+                    textBox1.Enabled = false;
+                    timer1.Start();
+                }
+                else
+                {
+                    MessageBox.Show("Tienes que escribir el nombre", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            label3.ForeColor = Color.FromArgb(r.Next(256), r.Next(256), r.Next(256));
         }
     }
 }
