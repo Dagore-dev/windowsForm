5e0e8574d34641b735e6991a5db9a28fac2f5514
Dagore <david.gomez.redondo.dev@gmail.com>
2022-04-19 15:48:18 +0200

    Estadísticas Texto

--- Relacion1/EstadisticasTexto/EstadisticasTexto/Form1.Designer.cs
+++ Relacion1/EstadisticasTexto/EstadisticasTexto/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace EstadisticasTexto
 {
-    partial class Form1
+    partial class EstadistiscasTexto
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,17 +29,17 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxLetters = new System.Windows.Forms.TextBox();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
             this.labelLetters = new System.Windows.Forms.Label();
             this.labelWords = new System.Windows.Forms.Label();
             this.labelSpaces = new System.Windows.Forms.Label();
             this.labelVowels = new System.Windows.Forms.Label();
             this.labelConsonants = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBoxConsonants = new System.Windows.Forms.TextBox();
+            this.textBoxVowels = new System.Windows.Forms.TextBox();
+            this.textBoxSpaces = new System.Windows.Forms.TextBox();
+            this.textBoxWords = new System.Windows.Forms.TextBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -56,17 +56,17 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 7F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 13F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 7F));
-            this.tableLayoutPanel1.Controls.Add(this.textBox6, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxLetters, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxInput, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.labelLetters, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.labelWords, 2, 1);
             this.tableLayoutPanel1.Controls.Add(this.labelSpaces, 4, 1);
             this.tableLayoutPanel1.Controls.Add(this.labelVowels, 6, 1);
             this.tableLayoutPanel1.Controls.Add(this.labelConsonants, 8, 1);
-            this.tableLayoutPanel1.Controls.Add(this.textBox2, 9, 1);
-            this.tableLayoutPanel1.Controls.Add(this.textBox3, 7, 1);
-            this.tableLayoutPanel1.Controls.Add(this.textBox4, 5, 1);
-            this.tableLayoutPanel1.Controls.Add(this.textBox5, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxConsonants, 9, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxVowels, 7, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxSpaces, 5, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxWords, 3, 1);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
@@ -75,24 +75,25 @@
             this.tableLayoutPanel1.Size = new System.Drawing.Size(776, 426);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
-            // textBox6
+            // textBoxLetters
             // 
-            this.textBox6.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.textBox6.Location = new System.Drawing.Point(103, 371);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.ReadOnly = true;
-            this.textBox6.Size = new System.Drawing.Size(45, 23);
-            this.textBox6.TabIndex = 10;
+            this.textBoxLetters.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBoxLetters.Location = new System.Drawing.Point(103, 371);
+            this.textBoxLetters.Name = "textBoxLetters";
+            this.textBoxLetters.ReadOnly = true;
+            this.textBoxLetters.Size = new System.Drawing.Size(45, 23);
+            this.textBoxLetters.TabIndex = 10;
             // 
-            // textBox1
+            // textBoxInput
             // 
-            this.tableLayoutPanel1.SetColumnSpan(this.textBox1, 10);
-            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox1.Location = new System.Drawing.Point(3, 3);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(770, 334);
-            this.textBox1.TabIndex = 0;
+            this.tableLayoutPanel1.SetColumnSpan(this.textBoxInput, 10);
+            this.textBoxInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxInput.Location = new System.Drawing.Point(3, 3);
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(770, 334);
+            this.textBoxInput.TabIndex = 0;
+            this.textBoxInput.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
             // 
             // labelLetters
             // 
@@ -144,50 +145,50 @@
             this.labelConsonants.TabIndex = 5;
             this.labelConsonants.Text = "Consonantes";
             // 
-            // textBox2
+            // textBoxConsonants
             // 
-            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.textBox2.Location = new System.Drawing.Point(719, 371);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(51, 23);
-            this.textBox2.TabIndex = 6;
+            this.textBoxConsonants.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBoxConsonants.Location = new System.Drawing.Point(719, 371);
+            this.textBoxConsonants.Name = "textBoxConsonants";
+            this.textBoxConsonants.ReadOnly = true;
+            this.textBoxConsonants.Size = new System.Drawing.Size(51, 23);
+            this.textBoxConsonants.TabIndex = 6;
             // 
-            // textBox3
+            // textBoxVowels
             // 
-            this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.textBox3.Location = new System.Drawing.Point(565, 371);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.ReadOnly = true;
-            this.textBox3.Size = new System.Drawing.Size(45, 23);
-            this.textBox3.TabIndex = 7;
+            this.textBoxVowels.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBoxVowels.Location = new System.Drawing.Point(565, 371);
+            this.textBoxVowels.Name = "textBoxVowels";
+            this.textBoxVowels.ReadOnly = true;
+            this.textBoxVowels.Size = new System.Drawing.Size(45, 23);
+            this.textBoxVowels.TabIndex = 7;
             // 
-            // textBox4
+            // textBoxSpaces
             // 
-            this.textBox4.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.textBox4.Location = new System.Drawing.Point(411, 371);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.ReadOnly = true;
-            this.textBox4.Size = new System.Drawing.Size(45, 23);
-            this.textBox4.TabIndex = 8;
+            this.textBoxSpaces.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBoxSpaces.Location = new System.Drawing.Point(411, 371);
+            this.textBoxSpaces.Name = "textBoxSpaces";
+            this.textBoxSpaces.ReadOnly = true;
+            this.textBoxSpaces.Size = new System.Drawing.Size(45, 23);
+            this.textBoxSpaces.TabIndex = 8;
             // 
-            // textBox5
+            // textBoxWords
             // 
-            this.textBox5.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.textBox5.Location = new System.Drawing.Point(257, 371);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.ReadOnly = true;
-            this.textBox5.Size = new System.Drawing.Size(45, 23);
-            this.textBox5.TabIndex = 9;
+            this.textBoxWords.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBoxWords.Location = new System.Drawing.Point(257, 371);
+            this.textBoxWords.Name = "textBoxWords";
+            this.textBoxWords.ReadOnly = true;
+            this.textBoxWords.Size = new System.Drawing.Size(45, 23);
+            this.textBoxWords.TabIndex = 9;
             // 
-            // Form1
+            // EstadistiscasTexto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "EstadistiscasTexto";
+            this.Text = "Estadísticas Texto";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
@@ -197,16 +198,16 @@
         #endregion
 
         private TableLayoutPanel tableLayoutPanel1;
-        private TextBox textBox6;
-        private TextBox textBox1;
+        private TextBox textBoxLetters;
+        private TextBox textBoxInput;
         private Label labelLetters;
         private Label labelWords;
         private Label labelSpaces;
         private Label labelVowels;
         private Label labelConsonants;
-        private TextBox textBox2;
-        private TextBox textBox3;
-        private TextBox textBox4;
-        private TextBox textBox5;
+        private TextBox textBoxConsonants;
+        private TextBox textBoxVowels;
+        private TextBox textBoxSpaces;
+        private TextBox textBoxWords;
     }
 }
--- Relacion1/EstadisticasTexto/EstadisticasTexto/Form1.cs
+++ Relacion1/EstadisticasTexto/EstadisticasTexto/Form1.cs
@@ -1,10 +1,72 @@
+﻿using System.Text;
+
 namespace EstadisticasTexto
 {
-    public partial class Form1 : Form
+    public partial class EstadistiscasTexto : Form
     {
-        public Form1()
+        public EstadistiscasTexto()
         {
             InitializeComponent();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            int letters = 0, spaces = 0, vowels = 0, consonants = 0, words = 0;
+            string[] lines = textBoxInput.Lines;
+
+            foreach (string line in lines)
+            {
+                words += CuentaPalabras(line);
+                foreach (char c in line)
+                {
+                    if (char.IsLetter(c))
+                    {
+                        letters++;
+                        if (IsVowel(c))
+                        {
+                            vowels++;
+                        }
+                        else
+                        {
+                            consonants++;
+                        }
+                    }
+                    else
+                    {
+                        if (char.IsWhiteSpace(c))
+                        {
+                            spaces++;
+                        }
+                    }
+                }
+            }
+
+            textBoxLetters.Text = letters.ToString();
+            textBoxSpaces.Text = spaces.ToString();
+            textBoxVowels.Text = vowels.ToString();
+            textBoxConsonants.Text = consonants.ToString();
+            textBoxWords.Text = words.ToString();
+        }
+        private bool IsVowel(char c)
+        {
+            string vowels = "aeiouyáéíóúýa̋e̋i̋őűàèìòùỳầềồḕṑǜừằȁȅȉȍȕăĕĭŏŭy̆ắằẳẵặḝȃȇȋȏȗǎěǐǒǔy̌a̧ȩə̧ɛ̧i̧ɨ̧o̧u̧âêîôûŷḙṷẩểổấếốẫễỗậệộäëïöüÿṳḯǘǚṏǟȫǖṻȧėıȯẏǡạẹịọụỵậȩ̇ǡȱảẻỉỏủỷơướứờừởửỡữợựāǣēīōūȳḗṓȭǭąęįǫųy̨åi̊ůḁǻą̊ãẽĩõũỹаэыуояеёюийⱥɇɨøɵꝋʉᵿɏөӫұɨαεηιοωυάέήίόώύὰὲὴὶὸὼὺἀἐἠἰὀὠὐἁἑἡἱὁὡὑᾶῆῖῶῦἆἦἶὦὖἇἧἷὧὗᾳῃῳᾷῇῷᾴῄῴᾲῂῲᾀᾐᾠᾁᾑᾡᾆᾖᾦᾇᾗᾧϊϋΐΰῒῢῗῧἅἕἥἵὅὥὕἄἔἤἴὄὤὔἂἒἢἲὂὢὒἃἓἣἳὃὣὓᾅᾕᾥᾄᾔᾤᾂᾒᾢᾃᾓᾣæɯɪʏʊøɘɤəɛœɜɞʌɔɐɶɑɒιυ";
+
+            return vowels.Contains(c);
+        }
+        private int CuentaPalabras(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            //Filtro para quedarte solo con letras y espacios en blanco
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsLetter(s[i]) || char.IsWhiteSpace(s[i]))
+                {
+                    sb.Append(s[i]);
+                }
+            }
+
+            return sb.ToString().Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
         }
     }
 }
