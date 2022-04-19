6711b3aa494fbb28dbed7f0d5bedb72d871e7927
Dagore <david.gomez.redondo.dev@gmail.com>
2022-04-19 22:53:34 +0200

    Cursor

---
+++ Relacion1/Cursor/Cursor/Form1.cs
@@ -0,0 +1,54 @@
+﻿namespace Cursor
+{
+    public partial class Cursor : Form
+    {
+        public Cursor()
+        {
+            InitializeComponent();
+        }
+
+        private void MoveCursor(object sender, EventArgs e)
+        {
+            Button btn = (Button) sender;
+            Point origin = new Point(240, 180);
+
+            switch (btn.Text)
+            {
+                case "✜":
+                    labelCursor.Location = origin;
+                    break;
+                case "⇖":
+                    labelCursor.Left -= 5;
+                    labelCursor.Top -= 5;
+                    break;
+                case "⇑":
+                    labelCursor.Top -= 5;
+                    break;
+                case "⇗":
+                    labelCursor.Left += 5;
+                    labelCursor.Top -= 5;
+                    break;
+                case "⇐":
+                    labelCursor.Left -= 5;
+                    break;
+                case "⇒":
+                    labelCursor.Left += 5;
+                    break;
+                case "⇙":
+                    labelCursor.Left -= 5;
+                    labelCursor.Top += 5;
+                    break;
+                case "⇓":
+                    labelCursor.Top += 5;
+                    break;
+                case "⇘":
+                    labelCursor.Left += 5;
+                    labelCursor.Top += 5;
+                    break;
+                default:
+                    labelCursor.Location = origin;
+                    break;
+            }
+        }
+    }
+}
